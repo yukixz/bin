7aa15cea5c738fcd8ba8aa2cfb9cde5e2c4f4d3d
Dazzy Ding <h@dazzyd.org>
2019-03-03 16:34:53 +0800

    Update twitter/daily.py

--- twitter/daily.py
+++ twitter/daily.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 
 import json
+import sys
+import traceback
 from datetime import datetime
 
-import pymysql
+import sqlite3
 from requests_oauthlib import OAuth1Session
 
 
 class Twitter():
     def __init__(self):
+        with open('daily.json') as f:
+            kwargs = json.loads(f.read())
         self.session = OAuth1Session(
-            client_key="{consumer_key}",
-            client_secret="{consumer_secret}",
-            resource_owner_key="{access_token}",
-            resource_owner_secret="{access_secret}",
-            )
+            client_key="",
+            client_secret="",
+            resource_owner_key="",
+            resource_owner_secret="",
+        )
 
     def crawl(self, last_id):
         ''' Crawl new tweet from user timeline.
@@ -46,27 +50,25 @@ class Twitter():
 
 class Database():
     def __init__(self):
-        self.connection = pymysql.connect(
-            host="localhost",
-            user="{mysql_user}",
-            password="{mysql_password}",
-            database="twitter",
-            charset="utf8mb4",
-            )
+        self.connection = sqlite3.connect("twitter.db", isolation_level=None)
+
+    def __del__(self):
+        # self.connection.commit()
+        self.connection.close()
 
     def insert(self, tweet):
         try:
-            tweet_id = tweet['id']
-            user_id = tweet['user']['id']
+            tweet_id = int(tweet['id'])
+            user_id = int(tweet['user']['id'])
             text = tweet['text']
             time = str(datetime.strptime(
                 tweet['created_at'].replace("+0000", ''), "%c"))
             cursor = self.connection.cursor()
             cursor.execute(
-                '''INSERT INTO statuses VALUES (%s, %s, %s, %s)''',
+                '''INSERT INTO statuses VALUES (?, ?, ?, ?)''',
                 (tweet_id, user_id, text, time))
-        except pymysql.err.ProgrammingError as err:
-            print("Fail to insert tweet: %d" % tweet_id, file=sys.stderr)
+        except sqlite3.Error as err:
+            print("Sqlite error inserting tweet %d" % tweet_id, file=sys.stderr)
             traceback.print_exc()
 
     def get_last_id(self):
